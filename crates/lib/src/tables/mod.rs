c667b825d2cdd02646953bfba7297e183e09ad3b
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-10-02 14:58:17 -0500

    wip!

--- crates/lib/src/tables/mod.rs
+++ crates/lib/src/tables/mod.rs
@@ -27,3 +27,124 @@ pub mod reg_locator;
 pub mod registry;
 pub mod signature;
 pub mod table_entry;
+
+// TODO: Look at Directory to see the form that I eventually want to have implemented.
+#[derive(strum::EnumIter)]
+enum Table {
+    ActionTable,
+    AdminExecuteSequence,
+    AdminUiSequence,
+    AdvtExecuteSquence,
+    AdvtUiSequence,
+    AppId,
+    AppSearch,
+    BBControl,
+    Billboard,
+    Binary,
+    BindImage,
+    CCPSearch,
+    CheckBox,
+    Class,
+    CoimboBox,
+    CompLocators,
+    Complus,
+    Component,
+    Condition,
+    Control,
+    ControlCondition,
+    ControlEvent,
+    CreateFolder,
+    CustomAction,
+    Dialog,
+
+    /// Directory layout for the application
+    ///
+    /// [*Reference*](https://learn.microsoft.com/en-us/windows/win32/msi/directory-table)
+    ///
+    // TODO: Implement the macros below as these are how I want to define the tables.
+    //
+    // // The MsiTable macro creates all of the boilerplate functions that depend on the name of
+    // // the table. The `#` next to `DirectoryIdentifier` indicates that this field is the primary
+    // // key in the table.
+    // //
+    // // For tables with identifiers that need to be generated before the MSI is created, the
+    // // IdentifierGenerator macro will create a [TABLE]Identifier struct that implments the
+    // // ToIdentifier trait.
+    // #[derive(MsiTable, IdentifierGenerator)]
+    // #[msi_table(
+    //     DefaultDir: Value::Str,
+    //     #DirectoryIdentifier: Value::Str,
+    //     Option<DirectoryIdentifier>: Value::Str
+    // )]
+    Directory,
+    DrLocator,
+    DuplicateFile,
+    Environment,
+    Error,
+    EventMapping,
+    Extensiuon,
+    Feature,
+    FeatureComponents,
+    File,
+    FileSFPCatqalog,
+    Font,
+    Icon,
+    IniFile,
+    IniLocator,
+    InstallExecuteSequence,
+    InstallUISequence,
+    IsolatedComponent,
+    LaunchCondition,
+    ListBox,
+    ListView,
+    LockPermissions,
+    Media,
+    MIME,
+    MoveFile,
+    MsiAssembly,
+    MsiAssemblyName,
+    MsiDigitalCertificate,
+    MsiDigitalSiganture,
+    MsiEmbeddedChainer,
+    MsiEmbeddedUI,
+    MsiFileHash,
+    MsiLockFilePermissionsEx,
+    MsiPackageCertificate,
+    MsiPatchCertificate,
+    MsiPatchHeaders,
+    MsiPatchMetadata,
+    MsiPatchOldAssemblyName,
+    MsiPatchOldAssemblyFile,
+    MsiPatchSequence,
+    MsiServiceConfig,
+    MsiServiceConfigFailureActions,
+    MsiSFFCBypass,
+    ODBCAttribute,
+    ODBCDataSource,
+    ODBCDrive,
+    ODBCSourceAttribute,
+    ODBCTranslator,
+    Patch,
+    PatchPackage,
+    ProgId,
+    Property,
+    PublishComponent,
+    RadioButton,
+    Reguistry,
+    RegLocator,
+    RemoveFile,
+    RemoveIniFile,
+    RemoveRegistry,
+    ReserveCost,
+    SelfReg,
+    ServiceControl,
+    ServiceInstall,
+    SFPCatalog,
+    Shortcut,
+    Signature,
+    TextStyle,
+    TypeLib,
+    UIText,
+    Verb,
+    Upgrade,
+}
